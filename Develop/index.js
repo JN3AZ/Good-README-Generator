7030da440942a5476b3fed1a15ab2558321c03ed
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-05-27 23:52:51 -0700

    added a README-template and started building my index.js page, added 3 const inquirer, fs, and template and wrote out my questions, then wrote out .then function

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,58 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+inquirer
+.prompt([
+    {
+        name:"name",
+        type: "input",
+        message: "What is your name?",
+    },
+    {
+        name: "aboutMe",
+        type: "input",
+        message: "What is something interesting about you?",
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "Please provide a good email to contact you",
+    },
+])
+.then(function ({ name, aboutMe, email }){
+    const template = `
+    # ${name}
 
-function writeToFile(fileName, data) {
-}
+    ## About Me
 
-function init() {
+    ${aboutMe}
 
-}
+    ## Contact Me
 
-init();
+    ${email}
+    
+    `;
+      fs.writeFile("README.md", template, function () {});
+});
+
+
+
+
+
+
+
+
+
+
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
---
+++ README-template.md
@@ -0,0 +1,9 @@
+# James Nowak
+
+## About me 
+
+I love music, cooking, and all things technology. I have always loved computers and have grown up with them but my new found love for coding is the most exciting thing I have ever begun to learn.
+
+## Contact Me 
+
+random1email@email.com
